0d7a33ef7a935b08eaca7664a6f804729d9522c1
Natali <Lifeismystery@yandex.ru>
2014-05-03 23:25:03 +0400

    v1.0

---
+++ AddOns/Gadgets/wtLibGadget/controls/wtControl.lua
@@ -0,0 +1,18 @@
+--[[
+                                G A D G E T S
+      -----------------------------------------------------------------
+                            wildtide@wildtide.net
+                           DoomSprout: Rift Forums 
+      -----------------------------------------------------------------
+      Gadgets Framework   : v0.6.0
+      Project Date (UTC)  : 2014-05-01T18:02:40Z
+      File Modified (UTC) : 2012-07-16T20:52:55Z (Wildtide)
+      -----------------------------------------------------------------     
+--]]
+
+local toc, data = ...
+local AddonId = toc.identifier
+local TXT = Library.Translate
+
+
+WT.Control = {}
